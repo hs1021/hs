9fbe8456aa3b920c05a17d2bddfcefcd883562c1
帅 <@192.168.137.246>
2019-10-22 14:23:36 +0800

    1.0

---
+++ xianmu/src/main/java/com/dzkj/util/Test.java
@@ -0,0 +1,9 @@
+package com.dzkj.util;
+
+public class Test {
+	public static void main(String[] args) {
+		String code = Model.sendCheckMSM("15580832295");
+		System.out.println("ÑéÖ¤ÂëÎª£º"+code);
+	}
+
+}
